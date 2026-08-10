c02efc2c1baf39cead84139ae1440bc521ebdf44
notanoob <cooperye09@gmail.com>
2026-08-09 20:19:18 -0500

    changes

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -113,6 +113,20 @@ require("lazy").setup({
 				},
 			},
 		},
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			keys = {
+				{
+					"<leader>?",
+					function()
+						require("which-key").show({ global = false })
+					end,
+					desc = "Buffer Local Keymaps (which-key)",
+				},
+			},
+		},
 	},
 	install = { colorscheme = { "catppuccin-nvim" } },
 	checker = { enabled = false },
